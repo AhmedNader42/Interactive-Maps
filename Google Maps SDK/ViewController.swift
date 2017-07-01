861a018f2a3024b7b5e237584332a178d9c70e04
Admin <admin@Admins-Mac.local>
2017-07-01 04:47:43 +0200

    Working map with a marker provided api key

--- Google Maps SDK/ViewController.swift
+++ Google Maps SDK/ViewController.swift
@@ -7,8 +7,23 @@
 //
 
 import UIKit
+import GoogleMaps
+
 
 class ViewController: UIViewController {
+    
+    
+    override func loadView() {
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view = mapView
+        
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
